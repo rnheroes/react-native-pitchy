78a42af108b3898f27659aea89fdbf5659c18865
Mia Loha.dev <mianalaloharano@gmail.com>
2025-04-23 20:55:19 +0300

    Create RNPitchySpec.h

---
+++ ios/RNPitchySpec.h
@@ -0,0 +1,37 @@
+#ifdef __cplusplus
+#pragma once
+#endif
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+/**
+ * Spec protocol for the Pitchy native module.
+ * This protocol defines the interface for the React Native JavaScript code to interact with.
+ */
+@protocol NativePitchySpec <RCTBridgeModule>
+
+/**
+ * Initialize the audio engine with the provided configuration.
+ */
+- (void)init:(NSDictionary *)config;
+
+/**
+ * Start pitch detection.
+ */
+- (void)start:(RCTPromiseResolveBlock)resolve
+       reject:(RCTPromiseRejectBlock)reject;
+
+/**
+ * Stop pitch detection.
+ */
+- (void)stop:(RCTPromiseResolveBlock)resolve
+      reject:(RCTPromiseRejectBlock)reject;
+
+/**
+ * Query if pitch detection is currently active.
+ */
+- (void)isRecording:(RCTPromiseResolveBlock)resolve
+            reject:(RCTPromiseRejectBlock)reject;
+
+@end 
